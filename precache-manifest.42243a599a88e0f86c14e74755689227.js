991c1154ac3afb3aeb649650dc3366cd0c835652
Cinthya Cabanzo <cinthya.cabanzo@gmail.com>
2020-10-21 21:02:54 -0500

    Updates

---
+++ precache-manifest.42243a599a88e0f86c14e74755689227.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cd0dd2495c0b6e94a646865016c71c2",
+    "url": "/market/index.html"
+  },
+  {
+    "revision": "4c2bb08c29abbb8aacc8",
+    "url": "/market/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c4d4db454757f75a7358",
+    "url": "/market/static/js/2.cfeb30f7.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/market/static/js/2.cfeb30f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c2bb08c29abbb8aacc8",
+    "url": "/market/static/js/main.25d2a8d1.chunk.js"
+  },
+  {
+    "revision": "a870e6736120b6fc972f",
+    "url": "/market/static/js/runtime-main.0e96747b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/market/static/media/logo.5d5d9eef.svg"
+  }
+]);
